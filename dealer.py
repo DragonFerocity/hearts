e24552bfbaea8a05424ab7865b0b48e28306b22c
Dragon Ferocity <rta120695@gmail.com>
2023-04-23 18:47:24 -0500

    Adding new AI submodule

---
+++ dealer.py
@@ -0,0 +1,18 @@
+# RULES OF HEARTS
+# 1. The simulated version of hearts in this file is the 4 player variant
+
+
+
+#1. Pick the players
+#2. Start play
+
+#Create the list of cards
+# 102 - 114: Clubs
+# 202 - 214: Diamonds
+# 302 - 314: Spades
+# 402 - 414: Hearts
+# Aces are high
+DECK = [102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, #Clubs
+        202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, #Diamonds
+        302, 303, 304, 305, 306, 307, 308, 309, 310, 311, 312, 313, 314, #Spades
+        402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414] #Hearts
